0cc21177bbbd6f9b5e24d64da3142c2d9e85ada3
havasig <havasi.gaabor@gmail.com>
2021-11-25 13:10:23 +0100

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,6 +7,13 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    if let databaseURL = Environment.get("DATABASE_URL") {
+        app.databases.use(try .postgres(
+                url: databaseURL
+        ), as: .psql)
+    }
+
+    /*
     app.databases.use(.postgres(
             hostname: Environment.get("DATABASE_HOST") ?? "localhost",
             port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
@@ -14,6 +21,8 @@ public func configure(_ app: Application) throws {
             password: Environment.get("DATABASE_PASSWORD") ?? "vapor",
             database: Environment.get("DATABASE_NAME") ?? "cocktail_database"
     ), as: .psql)
+     */
+
 
     app.migrations.add(CreateAlcoholic())
     app.migrations.add(CreateCategory())
